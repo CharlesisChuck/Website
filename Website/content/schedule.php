964fbb48539d4397af1a2ad9827aa98c45db4859
Charles <curt.charles.ee@gmail.com>
2018-11-04 11:51:11 -0800

    Schedule Progress
    Still can't think of proper structure for scheduel

--- Website/content/schedule.php
+++ Website/content/schedule.php
@@ -1,8 +1,84 @@
 <div class="left-col">
+<?php 
+include("scripts/schedule_functions.php");
+?>
+<h2>Your Schedule</h2>
 
-<h3>Coming Soon!</h3>
+<div class="schedule-edit">
+	<p><strong>Time Since Last Edit: </strong></p>
+	<ul>
+
+		<h3>Hours</h3>
+		<?php 
+			
+		$total_items = 3;
+		$first = 'Total Hours';
+		$second = 'Name';
+		$third = 'Remaining';
+		
+		TableSchedule($first,$second,$third,$total_items); 
+		?>
+
+		<h3>Daily Tasks</h3>
+		<table>
+			<tr>
+				<th>Task</th>
+				<th>Importance</th>
+				<th>Complete?</th>
+			</tr>
+			<?php
+			$task = 'do laundry';
+			$name_task = 3;
+			$complete = 'no';
+			$total_items = 4;
+			$i = 0;
+			for($i=0;$i<$total_items;$i++)
+			{
+				//$Total = function();
+				//$Name = function();
+				//$Remaining = function();
+			echo '
+			<tr>
+				<th>'.$task.'</th>
+				<th>'.$name_task.'</th>
+				<th>'.$complete.'</th>
+			</tr>
+			';
+			}
+			?>
+		</table>
+		<br><br/>
+
+		<h3>Weekly Tasks</h3>
+
+		</table>
+		<br><br/>
+
+		<h3>Months Tasks</h3>
+
+
+		<h3>Yearly Tasks</h3>
+
+	</ul>
+
+
+
+
+
+</div>
+
+
+
+
 
 <?php
+// $save_schedule = "not set";
+// if (empty($_POST[save_schedule])) {
+//     } else
+//     { 	
+//         $save_schedule = $_POST['notes'.$list_id];
+//         UpdateSchedule($save_schedule,$user_id,$db);
+//     }
 
 //USER ID
 //GET DATA FOR SCHEDULE
--- Website/scripts/schedule_functions.php
+++ Website/scripts/schedule_functions.php
@@ -1,70 +1,112 @@
 <?php
 //ALL CALCULATIONS WILL GO HERE
 
-function CreateSchedule($user_id,)
-{
-	$SizeHours = SizeData();
-	$SizeTasks = SizeData();
+// function CreateSchedule($user_id,)
+// {
+// 	$SizeHours = SizeData();
+// 	$SizeTasks = SizeData();
 
-	for ($SizeHours)
-	{
-		$category = 'hours';
-		GetScheduleData($db,$category,$logged_in_user_id)
-	}
-	for ($SizeTasks)
-	{
-		$category = 'tasks';
-		GetScheduleData($db,$category,$logged_in_user_id)
-	}
+// 	for ($SizeHours)
+// 	{
+// 		$category = 'hours';
+// 		GetScheduleData($db,$category,$logged_in_user_id)
+// 	}
+// 	for ($SizeTasks)
+// 	{
+// 		$category = 'tasks';
+// 		GetScheduleData($db,$category,$logged_in_user_id)
+// 	}
 
-}
+// }
 
-Function CurrentTime($db,$logged_in_user_id,)
-{
-	$DecodedTimeString = 0;
-	$TimeString = "";
+// Function CurrentTime($db,$logged_in_user_id,)
+// {
+// 	$DecodedTimeString = 0;
+// 	$TimeString = "";
 
-	$TimeString = GetTime();
+// 	$TimeString = GetTime();
 
-	//Decode the $TimeString
+// 	//Decode the $TimeString
 
-	return $DecodedTimeString;
-}
+// 	return $DecodedTimeString;
+// }
 
 
-//this creates and decodes the tasks strings
-function HourString($action,)
-{
-	$HourString = "";
-	if($action == 'create')
-	{
+// //this creates and decodes the tasks strings
+// function HourString($action,)
+// {
+// 	$HourString = "";
+// 	if($action == 'create')
+// 	{
 
-	}
-	else if($action == 'decode')
-	{
+// 	}
+// 	else if($action == 'decode')
+// 	{
 
-		//must be posted
-	}
+// 		//must be posted
+// 	}
 	
-	return $HourString;
-}
+// 	return $HourString;
+// }
 
-//this creates and decodes the tasks strings
-function TaskString($action,)
-{
-	$TaskString = "";
-	if($action == 'create')
-	{
+// //this creates and decodes the tasks strings
+// function TaskString($action,)
+// {
+// 	$TaskString = "";
+// 	if($action == 'create')
+// 	{
 
-	}
-	else if($action == 'decode')
-	{
+// 	}
+// 	else if($action == 'decode')
+// 	{
 
-		//must be posted
-	}
+// 		//must be posted
+// 	}
 	
-	return $TaskString;
+// 	return $TaskString;
+// }
+
+
+
+function TableSchedule($first,$second,$third,$total_items)
+{			
+		$first_data = 10;
+		$second_data = 'second';
+		$third_data = 3;
+
+		echo'
+				<table>
+				<tr>
+					<th>'.$first.'</th>
+					<th>'.$second.'</th>
+					<th>'.$third.'</th>
+				</tr>';
+
+			
+
+			$i = 0;
+			for($i=0;$i<$total_items;$i++)
+			{
+				// $first_data = $_POST['total_hour'];
+				// $second_data = $_POST['previous_hour_name'];
+				// $third_data = $_POST['current_hour'];
+		echo '
+				<tr>
+					<th><form method="post" >
+					<textarea rows="1" cols="2" name="hours">'.$first_data.'</textarea>
+					</form></th>
+
+					<th><form method="post" >
+					<textarea rows="1" cols="10" name="hours">'.$second_data.'</textarea>
+					</form></th>
+
+					<th><form method="post" >
+					<textarea rows="1" cols="2" name="hours">'.$third_data.'</textarea>
+					</form></th>
+				</tr>';
+			}
+		echo '</table><br><br/>';
 }
-
-
 ?>
+
+
